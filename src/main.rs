11a2e0217286d25eafc011c57b5fb881552f3a49
Veikko Soininen <veikko.soininen@gmail.com>
2022-07-07 17:37:35 +0300

    Implement HTTP GET in another thread

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-use std::error::Error;
+use std::sync::mpsc;
+use std::thread;
 
 mod types;
 
-async fn get() -> Result<types::Btc, Box<dyn Error>> {
-    Ok(
-        reqwest::get("https://api.coindesk.com/v1/bpi/currentprice.json")
-            .await?
-            .json::<types::Btc>()
-            .await?,
-    )
-}
+fn main() {
+    let (tx, rx) = mpsc::channel();
 
-#[tokio::main]
-async fn main() {
-    let resp = get().await;
-    match resp {
-        Ok(result) => println!("1 BTC = {:.0} €", result.bpi["EUR"].rate_float),
-        Err(err) => println!("Error happened: {}", err),
+    thread::spawn(move || {
+        let result = reqwest::blocking::get("https://api.coindesk.com/v1/bpi/currentprice.json");
+        match result {
+            Ok(response) => {
+                tx.send(response.json::<types::Btc>()).unwrap();
+            }
+            Err(err) => {
+                tx.send(Err(err)).unwrap();
+            }
+        };
+    });
+
+    for resp in rx.recv() {
+        match resp {
+            Ok(result) => println!("1 BTC = {:.0} €", result.bpi["EUR"].rate_float),
+            Err(err) => println!("Error happened: {}", err),
+        }
     }
 }
