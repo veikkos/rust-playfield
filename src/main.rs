a544e81f9c667edc08541ea89d71742ce5936ef9
Veikko Soininen <veikko.soininen@gmail.com>
2022-07-09 15:42:42 +0300

    Implement velocity conversion trait

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,21 @@ use measurement::Measurement;
 mod force;
 use force::*;
 
+trait VelocityConversions {
+    fn ms_to_kmh(&self) -> Self;
+    fn kmh_to_ms(&self) -> Self;
+}
+
+impl VelocityConversions for f64 {
+    fn ms_to_kmh(&self) -> f64 {
+        self * 3.6
+    }
+
+    fn kmh_to_ms(&self) -> f64 {
+        self / 3.6
+    }
+}
+
 fn manipulate_simulation(
     time_s: f64,
     pid: &mut Controller,
@@ -42,7 +57,7 @@ fn main() {
     loop {
         let drag = f_drag(velocity_ms);
         let gradient_force = f_gradient(weight, gradient_angle);
-        let pid_output = pid.step(velocity_ms, velocity_desired_kmh / 3.6);
+        let pid_output = pid.step(velocity_ms, velocity_desired_kmh.kmh_to_ms());
         let throttle_position = pid.clamp_and_normalize(pid_output);
         let force_forward = throttle_position * f_engine(velocity_ms) - drag - gradient_force;
         let acc = force_forward / weight;
